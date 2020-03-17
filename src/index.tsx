93e50833fbedc3d9a9005459d50fa21daf246554
nextdesusu <nextdesusu@gmail.com>
2020-03-17 19:57:59 +0400

    feat: add redux devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 import * as serviceWorker from "./serviceWorker";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { LSsave } from "./localStorage";
 import App from "./components/App";
 import reducer from "./reducer";
 import "./index.css";
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 store.subscribe(() => {
-    const state = store.getState();
-    LSsave(state);
-    console.log(state);
+  const state = store.getState();
+  LSsave(state);
 });
 
 export default store;
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-document.getElementById("root"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
